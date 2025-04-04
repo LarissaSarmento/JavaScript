4f903f94ddca62b965005cf4390db99da383a774
LarissaSarmento <larissalicurgoaltoe@outlook.com>
2025-04-03 21:29:47 -0300

    if

---
+++ aula11/ex008.js
@@ -0,0 +1,14 @@
+var v1 = 60.5
+console.log(`A velocidade do seu carro é igual a ${v1}`)
+if(v1>60){
+    console.log("Ultrapassou a velocidade")
+}
+
+///////////////////////
+
+var pais = 'EUA'
+if(pais == 'Brasil'){
+    console.log('Brasileiro')
+}else {
+    console.log('Estrangeiro')
+}
