dd4538405812f3c493c5ef9c73e01410126436ad
LarissaSarmento <larissa.altoe@atualsistemas.local>
2025-04-09 17:14:49 -0300

    aula15

---
+++ aula15/ambiente.js
@@ -0,0 +1,10 @@
+var c = 1 
+while (c <= 6){ // Estrutura de Repetição com teste lógico no início, primeiro faz o teste, sendo verdadeiro ele faz o bloco.
+    console.log(`Passo ${c}`)
+    c++
+}
+
+do { //Primeiro executa o bloco e depois faz o teste lógico. Faça enquanto
+    console.log(`Passo ${c}`)
+    c++
+} while(c <= l)
