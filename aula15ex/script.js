574af2bb7abd3c2ebcb5d2c035847c9790e8f532
LarissaSarmento <larissa.altoe@atualsistemas.local>
2025-04-10 17:01:21 -0300

    aula15ex

---
+++ aula15ex/script.js
@@ -0,0 +1,19 @@
+function contar() {
+    let ini = document.querySelector('#txti')
+    let fim = document.querySelector('#txtf')
+    let passo = document.querySelector('#txtp')
+    let res = document.querySelector('#res')
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        window.alert('[ERRO] Faltam dados!')
+    } else {
+        res.innerHTML = 'Contando...'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+
+        for(let c = 1; c <= f; c += p){
+            res.innerHTML += `${c}`
+        }
+    }
+}
