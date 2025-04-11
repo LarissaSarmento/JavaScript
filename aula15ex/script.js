8dfd185da5a88204be292cb89c98b5cecdfd5279
LarissaSarmento <larissa.altoe@atualsistemas.local>
2025-04-11 15:45:53 -0300

    ex15

--- aula15ex/script.js
+++ aula15ex/script.js
@@ -5,15 +5,29 @@ function contar() {
     let res = document.querySelector('#res')
 
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
-        window.alert('[ERRO] Faltam dados!')
+        // window.alert('[ERRO] Faltam dados!')
+        res.innerHTML = `Impossível contar!`
     } else {
         res.innerHTML = 'Contando...'
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
-
-        for(let c = 1; c <= f; c += p){
-            res.innerHTML += `${c}`
+        if (p <= 0) {
+            window.alert(`Passo inválido! Considerando PASSO 1`)
+            p - 1
         }
+        // Contagem crescente
+        if(i < f) {
+            for(let c = 1; c <= f; c += p) {
+                res.innerHTML += `${c} \u{1F449}` // Emoji
+            }
+
+        } else {
+        // Contagem regressiva
+            for(let c = i; c >= f; c -= p ) {
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
     }
 }
