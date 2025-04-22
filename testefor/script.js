3a853ac5a7e56cd8863484a82d77d27b19e0a211
LarissaSarmento <larissa.altoe@atualsistemas.local>
2025-04-22 17:05:17 -0300

    teste

---
+++ testefor/script.js
@@ -0,0 +1,18 @@
+function numeros() {
+    let num = document.querySelector('#num')
+    let res = document.querySelector('#res')
+
+    if (num.value.length == 0 ){
+        window.alert('Número inválido')
+    } else {
+        n = Number(num.value)
+        res.innerHTML= ''
+        if(n % 2 == 0){
+            for(n = 1; n <= 20; n++){
+                res.innerHTML += (` ${n}`)
+               } 
+        }else {
+            res.innerHTML = ('Número Ímpar')
+        }     
+    }
+}
