804352567dc9f35ff012bab58f9d1ab7a2ac2341
LarissaSarmento <larissa.altoe@atualsistemas.local>
2025-05-30 14:53:30 -0300

    teste013

---
+++ MDN/aula01/array/ExercicioArray/script.js
@@ -0,0 +1,27 @@
+let list = document.querySelector('. output ul')
+let totalBox = document.querySelector('.output p')
+let total = 0
+list.innerHTML = ''
+totalBox.textContent = ''
+//number 1
+'Underpants:6.99'
+                'Socks:5.99'
+                'T-shirt:14.99'
+                'Trousers:31.99'
+                'Shoes:23.99'
+
+for (let i = 0; i<= 0; i++){ // number 2
+    // number 3
+
+    // number 4
+    itemText = 0
+
+    let listItem = document.createElement('li')
+    listItem.textContent = itemText
+    list.appendChild(listItem)
+
+}
+
+totalBox.textContent = 'Total: $' + total.toFixed(2)
+
+
--- MDN/aula01/array/script.js
+++ MDN/aula01/array/script.js
@@ -12,11 +12,22 @@ let myNewString = myArray.join(',')
 // myArray[0]
 // let teste = myArray[1]
 //  myArray[myArray.length - 1]
+myArray.push('teste') // Insere Item no ultimo indice
 
-// console.log(myArray)
+let removerItemFinal = myArray.pop() // Remover o ultimo item do array
+
+myArray.unshift('InicioDoArray')
+
+let removerItemInicio = myArray.shift()
+
+console.log(removerItemFinal)
+console.log(myArray)
+console.log(removerItemInicio)
+
+// -----------------------------------------------
 
 let email = "teste@teste.com.br"
 let partes = email.split('@')
 let mascarado = partes[0].slice(0, 2) + "***@" + partes[1]
 
-console.log(mascarado)
+// console.log(mascarado)
