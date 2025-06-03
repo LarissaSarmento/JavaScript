d68dc45dd50b0da46911a1c1ad689df2c54d9128
LarissaSarmento <larissalicurgoaltoe@outlook.com>
2025-06-03 06:21:19 -0300

    teste012

---
+++ MDN/aula01/PraticaFinal/script.js
@@ -0,0 +1,55 @@
+// 1. COMPLETE VARIABLE AND FUNCTION DEFINITIONS
+let customName = document.querySelector('#customname')
+let randomize = document.querySelector('#randomize')
+let story = document.querySelector('.story')
+
+function randomValueFromArray(array){
+    const random = Math.floor(Math.random()*array.length)
+    return array[random]
+}
+
+// 2. RAW TEXT STRINGS
+
+let storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.'
+
+let insertX = ['Willy the Goblin',
+'Big Daddy',
+'Father Christmas']
+
+let insertY = ['the soup kitchen',
+'Disneyland',
+'the White House']
+
+let insertZ = ['spontaneously combusted',
+'melted into a puddle on the sidewalk',
+'turned into a slug and crawled away']
+
+// 3. EVENT LISTENER AND PARTIAL FUNCTION DEFINITION
+
+randomize.addEventListener('click', result)
+
+function result(){
+    if (customName.value !== ''){
+        const name = customName.value
+        newStory =  newStory.replace('Bob', name)
+    }
+
+    if(document.getElementById('uk').checked){
+        const weight = Math.round(300)
+        const temperature = Math.round(94)
+    }
+
+    let newStory = storyText
+    let xItem = randomValueFromArray
+    let yItem = randomValueFromArray
+    let zItem = randomValueFromArray
+
+    let x = newStory.replace(':insertx:', xItem)
+    let y = newStory.replace(':inserty:', yitem)
+    let z = newStory.replace(':insertz:', zitem)
+
+    console.log(x)
+
+    story.textContent = 
+    story.computedStyleMap.visibility = 'visible'
+}
