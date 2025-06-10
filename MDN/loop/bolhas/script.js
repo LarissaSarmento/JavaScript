f8e54ea7c7cce849a674b461f5a21d38165e9bf9
LarissaSarmento <larissalicurgoaltoe@outlook.com>
2025-06-10 06:45:07 -0300

    teste015

--- MDN/loop/bolhas/script.js
+++ MDN/loop/bolhas/script.js
@@ -1,17 +1,25 @@
-// for(let i = 0 < i < 100; i++){
-//  ctx.beginPath()
-//  ctx.fillStyle= "rgba(255,0,0,0.5)"
-//  ctx.arc(ramdom(WIDTH), random(HEIGHT), random(50), 0, 2 * Math.PI)
-//  ctx.fill()
-// }
+   var btn = document.querySelector("button");
+      var canvas = document.querySelector("canvas");
+      var ctx = canvas.getContext("2d");
 
+      var WIDTH = document.documentElement.clientWidth;
+      var HEIGHT = document.documentElement.clientHeight;
 
-var cats = ["Bill", "Jeff", "Pete", "Biggles", "Jasmin"];
-var info = "My cats are called ";
-var para = document.querySelector("#p");
+      canvas.width = WIDTH;
+      canvas.height = HEIGHT;
 
-for (var i = 0; i < cats.length; i++) {
-  info += cats[i] + ", ";
-}
+      function random(number) {
+        return Math.floor(Math.random() * number);
+      }
 
-para.textContent = info
+      function draw() {
+        ctx.clearRect(0, 0, WIDTH, HEIGHT);
+        for (var i = 0; i < 100; i++) {
+          ctx.beginPath();
+          ctx.fillStyle = "rgba(0, 119, 255, 0.5)";
+          ctx.arc(random(WIDTH), random(HEIGHT), random(50), 0, 2 * Math.PI);
+          ctx.fill();
+        }
+      }
+
+      btn.addEventListener("click", draw);
