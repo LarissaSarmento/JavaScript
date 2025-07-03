abd6f72665484a58254f3e39ec05dfe2da1c2a19
LarissaSarmento <larissalicurgoaltoe@outlook.com>
2025-07-03 05:55:24 -0300

    eventosObjeto

--- MDN/eventos/script.js
+++ MDN/eventos/script.js
@@ -4,7 +4,19 @@ function random(number){
   return Math.floor(Math.random() * (number + 1))
 }
 
-btn.onclick = function(){
+/* btn.addEventListener('click', () => {
   let rndCol = 'rgb('+ random(255) + ',' + random(255) + ',' + random(255) + ')'
   document.body.style.backgroundColor = rndCol
+})
+  */
+
+// OBJETOS DE EVENTO
+
+function bgChange(e) {
+  var rndCol =
+    "rgb(" + random(255) + "," + random(255) + "," + random(255) + ")";
+  e.target.style.backgroundColor = rndCol;
+  console.log(e);
 }
+
+btn.addEventListener("click", bgChange);
