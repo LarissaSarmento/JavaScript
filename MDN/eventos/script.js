fe2e77d311ff19f9ad5c11252539cb635a72da12
LarissaSarmento <larissa.altoe@atualsistemas.local>
2025-07-04 16:13:22 -0300

    projetogaleria

--- MDN/eventos/script.js
+++ MDN/eventos/script.js
@@ -20,3 +20,11 @@ function bgChange(e) {
 }
 
 btn.addEventListener("click", bgChange);
+
+/*
+O Objeto de Eventos é o seguinte, é passado um parâmetro, 
+e já é usado esse parametro e com o ponto acessa as propriedades direto do navegador como e.target
+
+Já uma função normal eu passo a função com parametro e uso a função e o parametro é uma variável
+
+*/
