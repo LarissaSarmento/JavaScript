1942317ab3af5da1eba7f7807ef66190bf31aae8
LarissaSarmento <larissa.altoe@atualsistemas.local>
2025-07-11 17:23:04 -0300

    contuacaoProjeto

--- MDN/ProjetoStore/main.js
+++ MDN/ProjetoStore/main.js
@@ -6,9 +6,30 @@ produtos.forEach(prod =>{
     const div = document.createElement('div')
     div.className = 'produto'
     div.innerHTML = `<img src="${prod.imagem}">
-    <h2>${prod.nome}<h2>
+    <h3>${prod.nome}<h3>
     <p>R$ ${prod.preco}</p>
-    <button>Adicionar ao Carrinho</button>`
+    <button id="adicionar">Adicionar ao Carrinho</button>`
 
     document.body.appendChild(div)
 })
+
+let carrinho = document.querySelector('#carrinho')
+let adicionar = document.querySelectorAll('#adicionar')
+let res = document.querySelector('#res')
+let total = 0
+
+adicionar.forEach(botao =>{
+    botao.addEventListener('click', (e) =>{
+        let preco = parseFloat(e.target.dataset.preco)
+        total += preco
+        res.textContent = `Total: ${total.toFixed(2)}`
+    })
+})
+
+
+// function adicionar(){
+//     if()
+// }
+
+// carrinho.addEventListener('click', ()=>{
+// })
