19260a0d0db935571f9792243df6b5e05793a150
LarissaSarmento <larissa.altoe@atualsistemas.local>
2025-07-16 17:27:14 -0300

    store003

--- MDN/ProjetoStore/main.js
+++ MDN/ProjetoStore/main.js
@@ -5,13 +5,29 @@ const produtos = [{nome: 'CanecaPinkFloyd', preco: 200.00, imagem: 'image/caneca
 produtos.forEach(prod =>{
     const div = document.createElement('div')
     div.className = 'produto'
+
+    div.dataset.nome = prod.nome
+    div.dataset.preco = prod.preco
+
     div.innerHTML = `<img src="${prod.imagem}">
     <h3>${prod.nome}<h3>
-    <p>R$ ${prod.preco}</p>
-    <button id="adicionar" data-preco="${prod.preco}">Adicionar ao Carrinho</button>`
+    <p class="preco">R$ ${prod.preco.toFixed(2)}</p>
+    <button class="addCar">Adicionar ao Carrinho</button>`
 
     document.querySelector('#produtos').appendChild(div)
 })
+
+let res = document.querySelector('#res')
+document.querySelector('#produtos').addEventListener('click', (e) =>{
+    const produto = e.target.closest('.produto')
+    if(!produto) return
+
+    const nome = produto.dataset.nome
+    const preco = parseFloat(produto.dataset.preco)
+
+    res.textContent= `Produto selecionado :${nome} - R$ ${preco.toFixed(2)}`
+})
+
 
 const carrinho = document.querySelector('#carrinho')
 const modal = document.querySelector('#modal')
@@ -26,16 +42,17 @@ fechar.addEventListener('click', () =>{
     modal.style.display = 'none'
 })
 
-let botoes = document.querySelectorAll('#adicionar')
-let res = document.querySelector('#res')
-let total = 0
-botoes.forEach(botao =>{
-    botao.addEventListener('click', (e) =>{
-        let preco = parseFloat(e.target.dataset.preco)
-        total += preco
-        res.textContent = `Total: ${total.toFixed(2)}`
-    })
-})
+
+
+// let botoes = document.querySelectorAll('#adicionar')
+// let total = 0
+// botoes.forEach(botao =>{
+//     botao.addEventListener('click', (e) =>{
+//         let preco = parseFloat(e.target.dataset.preco)
+//         total += preco
+//         res.textContent = `Total: ${total.toFixed(2)}`
+//     })
+// })
 
 
 // function adicionar(){
