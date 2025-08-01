40dd458c7d6eb9313c0bc5af14cab0be8d3162e1
LarissaSarmento <larissa.altoe@atualsistemas.local>
2025-08-01 17:04:18 -0300

    pratica010825

--- MDN/objeto/main.js
+++ MDN/objeto/main.js
@@ -1,1 +1,23 @@
-let pessoa = {}
+let pessoa = {
+    nome: ['Bob', 'Smith'],
+    idade: 32,
+    sexo: 'masculino',
+    interesse: ['musica', 'esquiar'],
+    bio: function () {
+        alert(
+        this.nome[0] + 
+        ' ' + 
+        this.nome[1] + 
+        ' tem ' + 
+        this.idade + 
+        ' anos de idade. Ele gosta de ' + 
+        this.interesses[0] + 
+        ' e ' + 
+        this.interesses[1] + 
+        '.',
+    )
+    },
+    saudacao: function () {
+        alert('Oi! eu sou' + this.nome[0] + '.')
+    }
+}
