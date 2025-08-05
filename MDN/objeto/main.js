28805ec53ad3bb4b84865d345483587f75eb68f0
LarissaSarmento <larissalicurgoaltoe@outlook.com>
2025-08-05 06:50:28 -0300

    pratica050825-1

--- MDN/objeto/main.js
+++ MDN/objeto/main.js
@@ -28,3 +28,49 @@ let pessoa = {
 let myDataName = 'altura'
 let myDataValue = '1.75m'
 pessoa[myDataName] = myDataValue
+
+
+
+/*
+Notação com ponto
+    - obj{nome = 'teste'
+    obj.nome = 32}
+
+    Nome aqui será 32
+
+
+Notação com colchetes
+    - obj{nome = 'teste'
+    obj[nome] = 32}
+
+    teste = 32, aqui o valor se torna uma variável e é usado no caso onde notação de ponto não atende
+    quando tem espaço, ífem, numeros com letras por ex:
+
+    obj{nome = 'teste sistema'
+    obj.[nome] = 85}
+    ou seja nome é teste sistema = 85
+    
+
+
+Notação com nome computado
+    -   Notação com nome computado é como criar um objeto e poder usar tanto notação de ponto quanto colchetes para ver o resultado 
+    let nome = 'teste'
+    let nomeAlterado = {
+    [nome]: 'teste02'}
+
+    reultado será nomeAlterado[nome]
+
+
+*/
+
+
+///////////////// Usando THIS /////////////////
+
+/*
+
+O THIS se adapta ao objeto que chama a função, por ex:
+
+Eu uso o this em um objeto dentro de uma função,
+nisso posso utilizar a mesma função em outro objeto, que ele vai trazer as informações do objeto atual
+
+*/
--- MDN/objeto/testObj/main.js
+++ MDN/objeto/testObj/main.js
@@ -1,62 +1,102 @@
 //Para visualizar eles individualmente só tirar comentário
 
-// OBJETO 01 /////////////////////////
+//OBJETO 01 /////////////////////////
+/*
+function infoGreeting (){
+    console.log(`Hello, said ${this.name} the ${this.breed}. `)
+}
 
-// const cat = {
-//     name: "Bertie",
-//     breed: "Cymric",
-//     color: "white",
-//     greeting: function () {
-//       console.log("Meow!");
-//     },
-//   };
+const cat = {
+    name: "Bertie",
+    breed: "Cymric",
+    color: "white",
+    greeting: infoGreeting
+  };
   
-//   // Don't edit the code above here!
+  // Don't edit the code above here!
   
-//   // Add your code here
+  // Add your code here
 
-//  let catName = cat['name']
-//  cat.greeting()
-//  cat.color = 'black'
-  
-//   // Don't edit the code below here!
-  
+  let cat2 = {
+    name: 'Gato',
+    breed: 'Raça',
+    color: 'black',
+    greeting: infoGreeting
+  }
 
-//   const section = document.querySelector("section");
-//   let para1 = document.createElement("p");
-//   let para2 = document.createElement("p");
+  let cat3 = {
+    name:'Lia',
+    breed:'Pura',
+    color: 'Marrom',
+    greeting: infoGreeting
+  }
+
+ let catName = cat['name']
+ cat.greeting()
+ cat2.greeting()
+ cat3.greeting()
+ cat.color = 'black'
+  */
+ 
+  // Don't edit the code below here!
+  
+////////// Toda essa parte de cima resumida aqui
+
+class Cat {
+    constructor(name, breed, color){
+        this.name = name
+        this.breed = breed
+        this.color = color
+    }
+    greeting(){
+        console.log(`Hello, said ${this.name} the ${this.breed}.`)
+    }
+}
+
+const cat = new Cat('Bertie', 'Cymric', 'white')
+const cat2 = new Cat('Elfie', 'Aphorodite Giant', 'ginger')
+
+
+  const section = document.querySelector("section");
+  let para1 = document.createElement("p");
+  let para2 = document.createElement("p");
 //   para1.textContent = `The cat's name is ${catName}.`;
-//   para2.textContent = `The cat's color is ${cat.color}.`;
-//   section.appendChild(para1);
-//   section.appendChild(para2);
+  para2.textContent = `The cat's color is ${cat.greeting()}.`;
+  section.appendChild(para1);
+  section.appendChild(para2);
   
 // OBJETO 02 /////////////////////////
 
-const band = {
-    name: 'Pink Floyd',
-    nationality: 'Britânia',
-    genre: 'Rock',
-    mebers: 4,
-    formed: 1965,
-    split: 2014,
-    albums: [
-    {name: 'The Piper at the Gares of Down', released: 1967},    
-    {name: 'The Wall', released: 1979}, 
-    {name: 'Whis You Were Here', released: 1975},
-    {name: 'The Dar Side od the Moon', relesead: 1973}
-]}
+// const band = {
+//     name: 'Pink Floyd',
+//     nationality: 'Britânia',
+//     genre: 'Rock',
+//     mebers: 4,
+//     formed: 1965,
+//     split: 2014,
+//     albums: [
+//     {name: 'The Piper at the Gares of Down', released: 1967},    
+//     {name: 'The Wall', released: 1979}, 
+//     {name: 'Whis You Were Here', released: 1975},
+//     {name: 'The Dar Side od the Moon', relesead: 1973}
+// ]}
 
-let bandInfo;
+// let bandInfo;
 
-// Don't edit the code above here!
+// // Don't edit the code above here!
 
-// Add your code here
-bandInfo = `A banda ${band.name}, nacionalidade ${band.nationality}, genero ${band.genre}, com ${band.mebers} membros, formada em ${band.formed},
-encerrou em ${band.split}, primeiro albúm ${band.albums[0].name} no ano de ${band.albums[1].released}, teste para encontrar ${band.albums?.[1]?.name?? 'Album inexistente'}`
+// // Add your code here
+// bandInfo = `A banda ${band.name}, nacionalidade ${band.nationality}, genero ${band.genre}, com ${band.mebers} membros, formada em ${band.formed},
+// encerrou em ${band.split}, primeiro albúm ${band.albums[0].name} no ano de ${band.albums[1].released}, teste para encontrar ${band.albums?.[1]?.name?? 'Album inexistente'}`
 
-// Don't edit the code below here!
+// // Don't edit the code below here!
 
-const section = document.querySelector("section");
-let para1 = document.createElement("p");
-para1.textContent = bandInfo;
-section.appendChild(para1);
+// const section = document.querySelector("section");
+// let para1 = document.createElement("p");
+// para1.textContent = bandInfo;
+// section.appendChild(para1);
+
+
+
+// OBJETO 03 /////////////////////////
+
